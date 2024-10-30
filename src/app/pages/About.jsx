df1e14bd3f63b9497deab31ec26d77c9850c32e0
Marouane <marouane@mbp-de-rui.home>
2024-10-30 11:29:58 +0100

    add new library for timeline animation

--- src/app/pages/About.jsx
+++ src/app/pages/About.jsx
@@ -2,8 +2,8 @@ import { motion, useInView } from "framer-motion";
 import Iphone from "../components/Phone";
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import '../../../node_modules/react-vertical-timeline-component/style.min.css'
-// import { AcademicCapIcon } from '@heroicons/react/solid';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBriefcase, faGraduationCap } from '@fortawesome/free-solid-svg-icons';
 
 import { useRef } from "react";
 
@@ -12,56 +12,74 @@ export default function AboutMeSection() {
     const inView = useInView(ref, { once: true });
   
     return (
-      <section className="min-h-screen flex items-center justify-center bg-green-500">
-        <motion.div
-          ref={ref}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: inView ? 1 : 0 }}
-          transition={{ duration: 1 }}
-          className="p-10 rounded"
-        >
-          <h2 className="text-2xl">About Me</h2>
-          <p>I'm a passionate web developer with experience in building dynamic applications.</p>
-        </motion.div>
+        <section className="flex w-full bg-green-500 p-10">
+        <div className="flex-1">
+            <VerticalTimeline>
+                <VerticalTimelineElement
+                    date="2022 - 2024"
+                    iconStyle={{ background: '#fff', color: '#000' }}
+                    icon={<FontAwesomeIcon icon={faGraduationCap} className="h-6 w-6 text-blue-500" />}
+                >
+                    <h3 className="text-lg font-bold">Mastère</h3>
+                    <p>INGÉNIERIE DU WEB</p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    date="2021 - 2022"
+                    iconStyle={{ background: '#fff', color: '#000' }}
+                    icon={<FontAwesomeIcon icon={faGraduationCap} className="h-6 w-6 text-blue-500" />}
+                >
+                    <h3 className="text-lg font-bold">Bachelor</h3>
+                    <p>INGÉNIERIE DU WEB</p>
+                </VerticalTimelineElement>
+            </VerticalTimeline>
+        </div>
 
-        <VerticalTimeline>
-            <VerticalTimelineElement
-                date="2020 - 2021"
-                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                // icon={<AcademicCapIcon className="h-6 w-6 text-blue-500" />}
-            >
-                <h3>Creative Director</h3>
-                <p>Creative Direction, User Experience, Visual Design</p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-                date="2011 - present"
-                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                // icon={<WorkIcon />}
-            >
-                <h3>Creative Director</h3>
-                <p>Creative Direction, User Experience, Visual Design</p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-                date="2011 - present"
-                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                // icon={<WorkIcon />}
-            >
-                <h3>Creative Director</h3>
-                <p>Creative Direction, User Experience, Visual Design</p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-                date="2011 - present"
-                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                icon={<i className="fas fa-code"></i>}
-            >
-                <h3>Creative Director</h3>
-                <p>Creative Direction, User Experience, Visual Design</p>
-            </VerticalTimelineElement>
-            {/* Ajoutez d'autres éléments ici */}x
-        </VerticalTimeline>
+        <div className="flex-1">
+            <VerticalTimeline>
+                <VerticalTimelineElement
+                    date="02/2024 - 09/2024"
+                    iconStyle={{ background: '#fff', color: '#000' }}
+                    icon={<FontAwesomeIcon icon={faBriefcase} className="h-6 w-6 text-blue-500" />}
+                >
+                    <h2 className="text-lg font-bold">Yomeva</h2>
+                    <h3 className="text-lg font-bold">Contrat en alternance</h3>
+                    <p>Développeur Full-stack</p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    date="09/2022 - 02/2024"
+                    iconStyle={{ background: '#fff', color: '#000' }}
+                    icon={<FontAwesomeIcon icon={faBriefcase} className="h-6 w-6 text-blue-500" />}
+                >
+                    <h2 className="text-lg font-bold">Foncia</h2>
+                    <h3 className="text-lg font-bold">Contrat en alternance</h3>
+                    <p>Développeur Full-stack</p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    date="03/2022 - 09/2022"
+                    iconStyle={{ background: '#fff', color: '#000' }}
+                    icon={<FontAwesomeIcon icon={faBriefcase} className="h-6 w-6 text-blue-500" />}
+                >
+                    <h2 className="text-lg font-bold">Thales</h2>
+                    <h3 className="text-lg font-bold">Contrat en alternance</h3>
+                    <p>Développeur Full-stack</p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    date="09/2021 - 03/2022"
+                    iconStyle={{ background: '#fff', color: '#000' }}
+                    icon={<FontAwesomeIcon icon={faBriefcase} className="h-6 w-6 text-blue-500" />}
+                >
+                    <h2 className="text-lg font-bold">Iviflo</h2>
+                    <h3 className="text-lg font-bold">Contrat en alternance</h3>
+                    <p>Développeur Full-stack</p>
+                </VerticalTimelineElement>
+            </VerticalTimeline>
+        </div>
+        </section>
 
-
-        {/* <motion.div
+    );
+  }
+  
+       {/* <motion.div
           ref={ref}
           initial={{ opacity: 0, x: 100 }}
           animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : 100 }}
@@ -70,8 +88,3 @@ export default function AboutMeSection() {
         >
           <Iphone screenshotSrc="/phone.webp" />
         </motion.div> */}
-      </section>
-    );
-  }
-  
-  
